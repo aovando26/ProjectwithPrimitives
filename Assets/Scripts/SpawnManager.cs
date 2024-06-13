27cdaa048edd3a808cbd9d80a3e0a61d112a7380
aovando <aovandodevsxr@gmail.com>
2024-06-13 11:35:35 -0400

    InvokeRepeating for delays and intervals

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,20 +9,28 @@ public class SpawnManager : MonoBehaviour
     private int intervalOfZ = 20;
     private int spawnPosZ = 10; 
     private float randomX;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f; 
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        InvokeRepeating("EnemySpawn", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
+    {
+
+    }
+
+    void EnemySpawn()
     {
         int randomX = Random.Range(-intervalOfZ, intervalOfZ);
 
         Vector3 posPrefab = new Vector3(randomX, 1, spawnPosZ);
 
         Instantiate(enemyPrefab, posPrefab, enemyPrefab.transform.rotation);
+
     }
 }
